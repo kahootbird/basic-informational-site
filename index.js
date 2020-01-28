312b5b8d851da20d01f0ff8aa45e1a751ecef303
Ronnie Goodrich <kahootbird@gmail.com>
2020-01-27 22:19:42 -0500

    Added links and url. Finished assingment.

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ http.createServer(function (req, res) {
   	filename = "index.html";
   fs.readFile(filename, function(err, data) {
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
+    			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end('404.html');
+  
+  } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
